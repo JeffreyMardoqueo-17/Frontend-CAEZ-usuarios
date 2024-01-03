3741c523064fb536df17895bda3c84cb424411b8
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2024-01-03 16:39:35 -0600

    Menu casi listo, no funciona el de close, tentgo que ponerlo en input tambien, entonces eso falta arreglar, cierra solo con el logo

--- src/components/MENU/Menu.jsx
+++ src/components/MENU/Menu.jsx
@@ -1,9 +1,6 @@
 import React, { useState } from 'react';
-import './Menu.css'
-//icono mennu
-import { IoMdMenu } from "react-icons/io";
-//icono exit 
-import { IoIosCloseCircleOutline } from "react-icons/io";
+import './Menu.css';
+import { IoMdMenu, IoIosCloseCircleOutline } from 'react-icons/io';
 
 const links = [
     { text: 'INICIO', href: '#', className: 'active' },
@@ -19,20 +16,35 @@ const Menu = () => {
         setMenuOpen(!menuOpen);
     };
 
+    const closeMenu = () => {
+        setMenuOpen(false);
+    };
+
+    const handleLinkClick = () => {
+        closeMenu();
+    };
+
     return (
         <div>
             <nav>
                 <input type="checkbox" id="check" checked={menuOpen} onChange={toggleMenu} />
                 <label htmlFor="check" className="checkbtn">
-                    {menuOpen ? <IoIosCloseCircleOutline className='icono' /> : <IoMdMenu className='icono' />}
+                    {menuOpen ? (
+                        <div className="icono-container" onClick={closeMenu}>
+                            <IoIosCloseCircleOutline className="icono" />
+                        </div>
+                    ) : (
+                        <IoMdMenu className="icono" />
+                    )}
                 </label>
-                <a href="#" className="enlace">
+                
+                <a href="#" className="enlace" onClick={handleLinkClick}>
                     <img src="/logo192.png" alt="Logo del cole" className="logo" />
                 </a>
                 <ul className={menuOpen ? 'show-menu' : ''}>
                     {links.map((link, index) => (
                         <li key={index}>
-                            <a href={link.href} className={link.className}>
+                            <a href={link.href} className={link.className} onClick={handleLinkClick}>
                                 {link.text}
                             </a>
                         </li>
@@ -42,6 +54,6 @@ const Menu = () => {
             <section></section>
         </div>
     );
-}
+};
 
 export default Menu;
