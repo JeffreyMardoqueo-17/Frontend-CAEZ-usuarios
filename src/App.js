3fb6dc76e2e30cb5df9e0abe4c6a1fd2a59d5df5
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2024-01-05 13:33:15 -0600

    Ya funciona la tabla de Mes

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Menu from './components/MENU/Menu';
 import Tabla from './components/MENU/Tabla/Tabla';
+import TablaMes from './components/MENU/TablaMes/TablaMes';
 function App() {
   return (
     <div className="App">
       <Menu />
       <Tabla />
-      <Tabla />
-      <Tabla />
-      <Tabla />
-      <Tabla />
+      <TablaMes />
     </div>
   );
 }
