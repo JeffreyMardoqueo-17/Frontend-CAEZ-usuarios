428ccd734894db9257799444ee6c3e421e8e2ffe
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2024-01-15 20:41:49 -0600

    Cambiar la funcion

--- src/Fuctions/GenerarPDF.js
+++ src/Fuctions/GenerarPDF.js
@@ -3,18 +3,21 @@ import 'jspdf-autotable';
 
 const generarPDF = (titulo, columnas, data, nombreArchivo) => {
     const doc = new jsPDF();
-
     // Fecha actual 
     const fechaActual = new Date();
     doc.text(titulo, 95, 20);
+    // Obtener un array de encabezados
+    const encabezados = columnas.map((col) => col.toUpperCase());
+
+    // Crear un array bidimensional con los encabezados y los datos
+    const datosTabla = [encabezados, ...data.map((row) => Object.values(row))];
 
     // Generar la tabla con los datos proporcionados
     doc.autoTable({
         startY: 30,
-        head: [columnas],  // <- Array unidimensional para la cabecera
-        body: data.map(row => Object.values(row))  // <- Array bidimensional para el cuerpo
+        head: [],  // <- No es necesario proporcionar head ya que se agregaraaa manualmente , todo jugado
+        body: datosTabla
     });
-
     // Guardar el PDF con el nombre proporcionado
     doc.save(`${nombreArchivo}, ${fechaActual.getDate()}-${fechaActual.getMonth() + 1}-${fechaActual.getFullYear()}.pdf`);
 };
