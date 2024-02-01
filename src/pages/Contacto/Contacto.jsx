a87a1c44fe09a9b89469f0d739614d8b3be1d051
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2024-02-01 11:23:07 -0600

    LA PARTE DE HUBICACION DERECHA E IZQUIERDA

--- src/pages/Contacto/Contacto.jsx
+++ src/pages/Contacto/Contacto.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
 import Menu from '../../components/MENU/Menu';
-
+import './Contacto.css'
 const Contacto = () => {
     return (
         <div>
-            <div>
-                <Menu />
-            </div>
-            <div className='align-content-center'>
-            <h1 className='text-center'>AQUI ESTAMOS EN EL CONTATO</h1>
+            <Menu />
+            <div className='conti'>
+                <div className='izquierda'>
+                </div>
+                <div className='derecha'>
+                    <div className="contacto-containes">
+                        <p className="title">Bienvenidos </p>
+
+                        <div className="separator"></div>
+                        <p className="welcome-message">
+                            Por favor, ingrese el número de NIE del estudiante para poder realizar la consulta de sus Pagos
+                        </p>
+                        <form className="login-form">
+                            <div className="form-controll">
+                                <input type="text" />
+                                <i className="fas fa-user"></i>
+                            </div>
+                            <button className="boton buscar radius" >
+                                Buscar
+                            </button>
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
     );
